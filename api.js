fece41e4938e31bc4dcd63ead2e31ac6f7151bd5
Stephen Whitmore <sww@eight45.net>
2018-04-18 17:20:42 -0700

    dev: flesh out api+routes with stubs

--- api.js
+++ api.js
@@ -3,25 +3,35 @@ var body = require('body/json')
 var JSONStream = require('JSONStream')
 var pump = require('pump')
 var ndjson = require('ndjson')
-var path = require('path')
-var hyperdb = require('hyperdb-osm')
 
-function Api (dir) {
-  var project = 'default'
-  this.db = hyperdb(path.join(dir, project))
+function Api (osm) {
+  this.osm = osm
 }
 
+// Observations
 Api.prototype.observationList = function (req, res, m) {
   var bbox = url.parse(req.url).query.bbox
   if (m.stream) parser = ndjson.stringify()
   else parser = JSONStream.stringify()
-  pump(this.db.observationStream(), parser, res, done)
+  pump(this.osm.observationStream(), parser, res, done)
   function done (err) {
     if (err) {
       res.statusCode = 500
       res.end(err)
     }
   }
+}
+
+Api.prototype.observationGet = function (req, res, m) {
+  this.osm.get(m.id, function (err, result) {
+    if (err) {
+      res.statusCode = 500
+      res.end(err)
+      return
+    }
+    res.setHeader('content-type', 'application/json')
+    res.end(JSON.stringify(values(result)))
+  })
 }
 
 Api.prototype.observationCreate = function (req, res, m) {
@@ -35,27 +45,53 @@ Api.prototype.observationCreate = function (req, res, m) {
   })
 }
 
-Api.prototype.observationGet = function (req, res, m) {
-  this.db.get(m.id, function (err, result) {
-    if (err) {
-      res.statusCode = 500
-      res.end(err)
-      return
-    }
-    res.setHeader('content-type', 'application/json')
-    res.end(JSON.stringify(values(result)))
-  })
-}
-
 Api.prototype.observationUpdate = function (req, res, m) {
-  // TODO
+  res.statusCode = 500
+  res.end('not implemented')
 }
 
+
+// Presets
+Api.prototype.presetsList = function (req, res, m) {
+  res.statusCode = 500
+  res.end('not implemented')
+}
+
+Api.prototype.presetsGet = function (req, res, m) {
+  res.statusCode = 500
+  res.end('not implemented')
+}
+
+// Media
 Api.prototype.mediaList = function (req, res, m) {
-  // TODO
+  res.statusCode = 500
+  res.end('not implemented')
 }
 
 Api.prototype.mediaGet = function (req, res, m) {
   var filename = m.filename
-  // TODO
+  res.statusCode = 500
+  res.end('not implemented')
+}
+
+Api.prototype.mediaPut = function (req, res, m) {
+  res.statusCode = 500
+  res.end('not implemented')
+}
+
+// Tiles
+Api.prototype.tilesList = function (req, res, m) {
+  res.statusCode = 500
+  res.end('not implemented')
+}
+
+Api.prototype.tilesGet = function (req, res, m) {
+  res.statusCode = 500
+  res.end('not implemented')
+}
+
+// Sync
+Api.prototype.syncAdb = function (req, res, m) {
+  res.statusCode = 500
+  res.end('not implemented')
 }
--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-var staticFiles = require('./staticFiles')
-var Api = require('./api')
 var Router = require('routes')
+var Api = require('./api')
 
 module.exports = function (dir) {
   var router = Router()
   var api = Api(dir)
 
-  router.addRoute('POST /observations', api.observationCreate.bind(api))
-  router.addRoute('GET /observations', api.observationList.bind(api))
-  router.addRoute('PUT /observation/:id', api.observationUpdate.bind(api))
-  router.addRoute('GET /observations/:id', api.observationGet.bind(api))
-  router.addRoute('GET /features', api.asFeatureCollection.bind(api))
+  // Observations
+  router.addRoute('GET /observations',      api.observationList.bind(api))
+  router.addRoute('GET /observations/:id',  api.observationGet.bind(api))
+  router.addRoute('POST /observations',     api.observationCreate.bind(api))
+  router.addRoute('PUT /observations/:id',  api.observationUpdate.bind(api))
 
-  router.addRoute('GET /media', api.mediaList.bind(api))
-  router.addRoute('GET /media/:filename', api.mediaGet.bind(api))
-  router.addRoute('GET /static/*', staticFiles)
+  // Presets
+  router.addRoute('GET /presets',           api.presetsList.bind(api))
+  router.addRoute('GET /presets/:id',       api.presetsGet.bind(api))
+
+  // Media
+  router.addRoute('GET /media',             api.mediaList.bind(api))
+  router.addRoute('GET /media/:id',         api.mediaGet.bind(api))
+  router.addRoute('PUT /media',             api.mediaPut.bind(api))
+
+  // Tiles
+  router.addRoute('GET /tiles',             api.tilesList.bind(api))
+  router.addRoute('GET /tiles/:id/:x/:y/:z',api.tilesGet.bind(api))
+
+  // Sync
+  router.addRoute('GET /sync/adb',          api.syncAdb.bind(api))
 
   return function (req, res) {
     var m = router.match(req.method + ' ' + req.url)
