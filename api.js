6b8ad95d216507ab72c752d0b000ca7826126fa4
Stephen Whitmore <sww@eight45.net>
2018-04-18 17:44:28 -0700

    dev: placeholder fixtures

--- api.js
+++ api.js
@@ -10,88 +10,67 @@ function Api (osm) {
 
 // Observations
 Api.prototype.observationList = function (req, res, m) {
-  var bbox = url.parse(req.url).query.bbox
-  if (m.stream) parser = ndjson.stringify()
-  else parser = JSONStream.stringify()
-  pump(this.osm.observationStream(), parser, res, done)
-  function done (err) {
-    if (err) {
-      res.statusCode = 500
-      res.end(err)
-    }
-  }
+  res.setHeader('content-type', 'application/json')
+  res.end(JSON.stringify([]))
 }
 
 Api.prototype.observationGet = function (req, res, m) {
-  this.osm.get(m.id, function (err, result) {
-    if (err) {
-      res.statusCode = 500
-      res.end(err)
-      return
-    }
-    res.setHeader('content-type', 'application/json')
-    res.end(JSON.stringify(values(result)))
-  })
+  res.setHeader('content-type', 'application/json')
+  res.end(JSON.stringify({ id: '123', lat: 12.3, lon: -0.522 }))
 }
 
 Api.prototype.observationCreate = function (req, res, m) {
-  body(req, res, function (err, doc) {
-    if (err) {
-      res.statusCode = 400
-      return res.end(err.message)
-    }
-    // CREATE DOC
-    res.end(JSON.stringify({id: 13}))
-  })
+  res.setHeader('content-type', 'application/json')
+  res.end(JSON.stringify({ id: '123', lat: 12.3, lon: -0.522 }))
 }
 
 Api.prototype.observationUpdate = function (req, res, m) {
-  res.statusCode = 500
-  res.end('not implemented')
+  res.setHeader('content-type', 'application/json')
+  res.end(JSON.stringify({ id: '123', lat: 12.3, lon: -0.522 }))
 }
 
 
 // Presets
 Api.prototype.presetsList = function (req, res, m) {
-  res.statusCode = 500
-  res.end('not implemented')
+  res.setHeader('content-type', 'application/json')
+  res.end(JSON.stringify(['default', 'jungle']))
 }
 
 Api.prototype.presetsGet = function (req, res, m) {
-  res.statusCode = 500
-  res.end('not implemented')
+  res.setHeader('content-type', 'application/json')
+  res.end(JSON.stringify({}))
 }
 
 // Media
 Api.prototype.mediaList = function (req, res, m) {
-  res.statusCode = 500
-  res.end('not implemented')
+  res.setHeader('content-type', 'application/json')
+  res.end(JSON.stringify([]))
 }
 
 Api.prototype.mediaGet = function (req, res, m) {
   var filename = m.filename
-  res.statusCode = 500
-  res.end('not implemented')
+  res.setHeader('content-type', 'application/json')
+  res.end(JSON.stringify({id: '23230', type: 'image/jpg', data: 'aGVsbG8gd29ybGQgdGhpcyBpcyBpbWFnZSBkYXRhCg=='}))
 }
 
 Api.prototype.mediaPut = function (req, res, m) {
-  res.statusCode = 500
-  res.end('not implemented')
+  res.setHeader('content-type', 'application/json')
+  res.end(JSON.stringify({id: '23230'}))
 }
 
 // Tiles
 Api.prototype.tilesList = function (req, res, m) {
-  res.statusCode = 500
-  res.end('not implemented')
+  res.setHeader('content-type', 'application/json')
+  res.end(JSON.stringify([]))
 }
 
 Api.prototype.tilesGet = function (req, res, m) {
-  res.statusCode = 500
-  res.end('not implemented')
+  res.setHeader('content-type', 'application/vnd.mapbox-vector-tile')
+  res.end(Buffer.alloc(12))
 }
 
 // Sync
 Api.prototype.syncAdb = function (req, res, m) {
-  res.statusCode = 500
-  res.end('not implemented')
+  // 200 OK
+  res.end()
 }
