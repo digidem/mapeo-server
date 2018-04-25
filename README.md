2f97dd4b8ee4d102894a001e2c14207bf6517afd
noffle <sww@eight45.net>
2018-04-25 10:14:04 -0700

    dev: use osm-p2p instead of hyperdb-osm

--- README.md
+++ README.md
@@ -124,20 +124,11 @@ device.
 ## Usage
 
 ```js
-var hyperdb = require('hyperdb')
-var hosm = require('hyperdb-osm')
-var sub = require('subleveldown')
-var grid = require('grid-point-store')
-var level = require('level')
+var Osm = require('osm-p2p')
 var blobstore = require('fs-blob-store')
 var Router = require('mapeo-mobile-server')
 
-var db = level('./index')
-var osm = hosm({
-  db: hyperdb('./db', { valueEncoding: 'json' }),
-  index: sub(db, 'idx'),
-  pointstore: grid(sub(db, 'geo'))
-})
+var osm = Osm('./db')
 var media = blobstore('./media')
 
 var route = Router(osm, media)
--- test/server.js
+++ test/server.js
@@ -1,20 +1,11 @@
-var hyperdb = require('hyperdb')
-var hosm = require('hyperdb-osm')
-var sub = require('subleveldown')
-var grid = require('grid-point-store')
-var memdb = require('memdb')
-var ram = require('random-access-memory')
+var Osm = require('osm-p2p-mem')
 var blob = require('abstract-blob-store')
 var http = require('http')
 var Router = require('..')
 
 module.exports = function (cb) {
   var db = memdb()
-  var osm = hosm({
-    db: hyperdb(ram, { valueEncoding: 'json' }),
-    index: sub(db, 'idx'),
-    pointstore: grid(sub(db, 'geo'))
-  })
+  var osm = Osm()
   var media = blob()
 
   var router = Router(osm, media)
