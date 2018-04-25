45ff19711e68334ebb499ea0b0e758904cf0de47
noffle <sww@eight45.net>
2018-04-25 10:49:46 -0700

    test: list observations

--- test/observations.js
+++ test/observations.js
@@ -93,3 +93,60 @@ test('observations: create + get', function (t) {
     })
   })
 })
+
+test('observations: create + list', function (t) {
+  createServer(function (server, base, osm, media) {
+    osm.create({lat:1,lon:2,type:'observation'}, function (err, id, node) {
+      t.error(err)
+
+      // create a fork of the above observation
+      var obs2 = {lat:2,lon:2,type:'observation'}
+      osm.batch([{type:'put',key:id,links:[],value:obs2}], function (err, nodes) {
+        t.error(err)
+        var expected = [
+          {
+            lat: 1,
+            lon: 2,
+            id: id,
+            type: 'observation',
+            version: node.key
+          },
+          {
+            lat: 2,
+            lon: 2,
+            id: id,
+            type: 'observation',
+            version: nodes[0].key
+          }
+        ]
+        check(expected)
+      })
+    })
+
+    function check (expected) {
+      var href = base + '/observations'
+
+      var hq = hyperquest.get(href, {
+        headers: { 'content-type': 'application/json' }
+      })
+
+      // http response
+      hq.once('response', function (res) {
+        t.equal(res.statusCode, 200, 'create 200 ok')
+        t.equal(res.headers['content-type'], 'application/json', 'type correct')
+      })
+
+      // response content
+      hq.pipe(concat({ encoding: 'string' }, function (body) {
+        try {
+          var objs = JSON.parse(body)
+          t.deepEquals(objs, expected, 'observation from server matches expected')
+        } catch (e) {
+          t.error(e, 'json parsing exception!')
+        }
+        server.close()
+        t.end()
+      }))
+    }
+  })
+})
