9cfd348a24c22274182e56c9b1e5b552fc340b65
Karissa McKelvey <krmckelv@gmail.com>
2019-02-15 14:31:14 -0800

    add test for http progress on starting replication

--- test/sync.js
+++ test/sync.js
@@ -1,5 +1,7 @@
 var test = require('tape')
 var needle = require('needle')
+var hyperquest = require('hyperquest')
+var through = require('through2')
 var {announce, unannounce, createServer, twoServers} = require('./server')
 
 test('sync - announce and close', function (t) {
@@ -29,6 +31,45 @@ test('sync - two-server announce and find eachother', function (t) {
           a.server.close()
           b.server.close()
           t.end()
+        })
+      })
+    })
+    announce(a, b, function (err) {
+      t.error(err)
+    })
+  })
+})
+
+test('sync - two-server sync', function (t) {
+  twoServers(function (a, b) {
+    function done () {
+      unannounce(a, b, function (err) {
+        t.error(err)
+        a.server.close()
+        b.server.close()
+        t.end()
+      })
+    }
+    a.router.api.core.sync.on('connection', function () {
+      needle.get(a.base + '/sync/targets', function (err, resp, body) {
+        t.error(err)
+        t.equal(body.length, 1)
+        var entry = body[0]
+        t.equal(entry.type, 'wifi')
+        var href = a.base + `/sync/start?host=${entry.host}&port=${entry.port}`
+        var hq = hyperquest(href, {end: false})
+        hq.pipe(through.obj(function (data, enc, next) {
+          var text
+          try {
+            text = JSON.parse(data.toString())
+          } catch (err) {
+            t.fail('JSON parse failed: ' + data.toString())
+          }
+          t.ok(RegExp(/replication-(complete|progress|started)/).test(text.topic))
+          next()
+        }))
+        hq.on('end', function () {
+          done()
         })
       })
     })
