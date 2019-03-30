5dd68bc72f9356c0457132218bbdee7bf9d7f877
Karissa McKelvey <krmckelv@gmail.com>
2019-03-30 14:34:11 -0600

    add tests for syncing to a file

---
+++ test/fileSync.js
@@ -0,0 +1,31 @@
+var test = require('tape')
+var path = require('path')
+var tmp = require('tmp')
+var hyperquest = require('hyperquest')
+var through = require('through2')
+var {createServer} = require('./server')
+
+test('file sync - create syncfile', function (t) {
+  var filename = tmp.fileSync()
+  createServer(function (server, base) {
+    function done (err) {
+      t.error(err)
+      server.close()
+      t.end()
+    }
+    var href = base + `/sync/start?filename=${filename}`
+    var hq = hyperquest(href, {end: false})
+    hq.pipe(through.obj(function (data, enc, next) {
+      var text
+      try {
+        text = JSON.parse(data.toString())
+      } catch (err) {
+        t.fail('JSON parse failed: ' + data.toString())
+      }
+      t.ok(RegExp(/replication-(complete|progress|started)/).test(text.topic))
+      next()
+    }))
+    hq.on('error', done)
+    hq.on('end', done)
+  })
+})
